fd5c9e7f16a68d8fc350cbd2aa56e8dbfd97e54b
bcerdas <bryan.cerdas07@gmail.com>
2025-09-16 19:35:08 -0600

    base de mi proyecto

---
+++ testing_repository/src/main/java/com/mycompany/testing_repository/Testing_repository.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testing_repository;
+
+/**
+ *
+ * @author bryan
+ */
+public class Testing_repository {
+
+    public static void main(String[] args) {
+        
+        int numero = 7;
+        
+        System.out.println("Soy un numero: " + numero);
+        
+    }
+}
