97e2dbe04f5fb682c284bb0e49b268b565bc4c00
bcerdas <bryan.cerdas07@gmail.com>
2025-09-16 20:00:49 -0600

    calculando el total

--- testing_repository/src/main/java/com/mycompany/testing_repository/Testing_repository.java
+++ testing_repository/src/main/java/com/mycompany/testing_repository/Testing_repository.java
@@ -16,5 +16,11 @@ public class Testing_repository {
         
         System.out.println("Soy un numero: " + numero);
         
+        int numero2 = 10;
+        int total = numero + numero2;
+        
+        System.out.println("El total es: " + total);
+        
+        
     }
 }
